be878044be1bca48b2af1367c6fa6a467a0af49d
badimala218 <badimala@ucsd.edu>
2026-02-04 20:45:53 -0800

    Added arrow helper function for retrieving HTML elements.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 window.document.addEventListener("DOMContentLoaded", () => {
     "use strict";
-    let form = document.getElementById("empForm");
-    let inputs = document.getElementsByTagName("input");
-    let dept = document.getElementById("department");
-    form.addEventListener("submit", (event) => {
+    const $ = (id) => document.getElementById(id);
+    $("empForm").addEventListener("submit", (event) => {
         event.preventDefault();
-        console.log("ID: " + inputs[0].value + "\n" + "Name: " + 
-                    inputs[1].value + "\n" + "Extension: " + 
-                    inputs[2].value + "\n" + "Email: " + 
-                    inputs[3].value + "\n" + "Department: " + dept.value);
+        console.log("ID: " + $("id").value + 
+                    "\nName: " + $("name").value +
+                    "\nExtension: " + $("ext").value + 
+                    "\nEmail: " + $("email").value + 
+                    "\nDepartment: " + $("department").value);
     }, false);
 }, false);
